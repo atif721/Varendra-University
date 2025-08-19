2caabcd79009ecc0530265017f3afafe9e06949f
atif721 <abdullahatif721@outlook.com>
2025-08-19 23:47:33 +0600

    Lab 4 added on CSE-2204

--- 04 Fourth Semester/CSE 2204/Lab reports/Lab-04-19.08.2025/BubbleSort.cpp
+++ 04 Fourth Semester/CSE 2204/Lab reports/Lab-04-19.08.2025/BubbleSort.cpp
@@ -1,25 +1,74 @@
 #include <iostream>
 using namespace std;
 
-void bubbleSort(int arr[], int n) {
-    for(int i = 0; i < n-1; i++) {
-        for(int j = 0; j < n-1-i; j++) {
-            if(arr[j] > arr[j+1]) {  // For ascending order
-                swap(arr[j], arr[j+1]);
+void printArray(int* array, int size) {
+    cout << "Array Elements are : ";
+    for (int i = 0; i < size; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
+
+void bubbleSort(int* array, int n) {
+    bool swapped;
+    int swapCount = 0;
+    int compareCount = 0;
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < n - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                swapped = true;
+                swapCount++;
             }
+            compareCount++;
+        }
+        if (swapped == false) {
+            break;
         }
     }
+    printArray(array, n);
+    cout << "Swapping : " << swapCount << endl;
+    cout << "Comparing : " << compareCount << endl;
 }
 
 int main() {
-    int arr[] = {5, 2, 9, 1, 5};
-    int n = sizeof(arr)/sizeof(arr[0]);
 
-    bubbleSort(arr, n);
+    // int size;
+    // cout << "Enter the array size : ";
+    // cin >> size;
 
-    cout << "Bubble Sorted Array: ";
-    for(int i = 0; i < n; i++) cout << arr[i] << " ";
-    cout << endl;
+    // int* array = new int[size];
+
+    // cout << "Enter array elements : ";
+    // for (int i = 0; i < size; i++) {
+    //     cin >> array[i];
+    // }
+
+    int array1[] = { 5,6,7,15,36,100 };
+    int array2[] = { 60,50,40,30,20,10 };
+    int array3[] = { 6,4,9,3,11,8,15,12 };
+
+    int size1 = sizeof(array1) / sizeof(array1[0]);
+    int size2 = sizeof(array2) / sizeof(array2[0]);
+    int size3 = sizeof(array3) / sizeof(array3[0]);
+
+    cout << "Before sorting ";
+    printArray(array1, size1);
+    cout << "After sorting ";
+    bubbleSort(array1, size1);
+
+    cout << "Before sorting ";
+    printArray(array2, size2);
+    cout << "After sorting ";
+    bubbleSort(array2, size2);
+
+    cout << "Before sorting ";
+    printArray(array3, size3);
+    cout << "After sorting ";
+    bubbleSort(array3, size3);
 
     return 0;
 }
--- 04 Fourth Semester/CSE 2204/Lab reports/Lab-04-19.08.2025/SelectionSort.cpp
+++ 04 Fourth Semester/CSE 2204/Lab reports/Lab-04-19.08.2025/SelectionSort.cpp
@@ -1,27 +1,62 @@
 #include <iostream>
 using namespace std;
 
-void selectionSort(int arr[], int n) {
-    for(int i = 0; i < n-1; i++) {
-        int minIndex = i;
-        for(int j = i+1; j < n; j++) {
-            if(arr[j] < arr[minIndex]) {  // For ascending order
-                minIndex = j;
-            }
-        }
-        if(minIndex != i) swap(arr[i], arr[minIndex]);
+void printArray(int* array, int size) {
+    cout << "Array Elements are : ";
+    for (int i = 0; i < size; i++) {
+        cout << array[i] << " ";
     }
+    cout << endl;
+}
+
+void selectionSort(int* array, int n) {
+    int swappingCount = 0;
+    int compareCount = 0;
+    int i, j;
+    for (i = 0; i < n - 1; i++) {
+        int min = i;
+        for (j = i + 1; j < n; j++) {
+            if (array[min] > array[j]) {
+                min = j;
+            }
+            compareCount++;
+        }
+        int temp = array[i];
+        array[i] = array[min];
+        array[min] = temp;
+        swappingCount++;
+    }
+    printArray(array, n);
+    cout << "Swapping : " << swappingCount << endl;
+    cout << "Comparing : " << compareCount << endl;
 }
 
 int main() {
-    int arr[] = {5, 2, 9, 1, 5};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    // srand(time(NULL));
+    cout << rand() % 3 + 2 << endl;
 
-    selectionSort(arr, n);
+    int array1[] = { 5,6,9,15,36,100 };
+    int array2[] = { 60,50,40,30,20,10 };
+    int array3[] = { 6,4,9,3,11,8,15,12 };
 
-    cout << "Selection Sorted Array: ";
-    for(int i = 0; i < n; i++) cout << arr[i] << " ";
-    cout << endl;
+    int size1 = sizeof(array1) / sizeof(array1[0]);
+    int size2 = sizeof(array2) / sizeof(array2[0]);
+    int size3 = sizeof(array3) / sizeof(array3[0]);
+
+    cout << "Before sorting ";
+    printArray(array1, size1);
+    cout << "After sorting ";
+    selectionSort(array1, size1);
+
+    cout << "Before sorting ";
+    printArray(array2, size2);
+    cout << "After sorting ";
+    selectionSort(array2, size2);
+
+    cout << "Before sorting ";
+    printArray(array3, size3);
+    cout << "After sorting ";
+    selectionSort(array3, size3);
 
     return 0;
 }
