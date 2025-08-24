21c7c7c26bb383e5210a099df390ba92bc6acfbb
atif721 <abdullahatif721@outlook.com>
2025-08-24 19:09:55 +0600

    Lab 4 added and Lab 3 updated

--- 04 Fourth Semester/CSE 2206/Lab 3 17.08.2025/bisection.py
+++ 04 Fourth Semester/CSE 2206/Lab 3 17.08.2025/bisection.py
@@ -3,18 +3,17 @@ from math import fabs
 def f(x):
     return 0.5*x*x*x - x*x
 
-def Bisection(f, xl, xu, max_itr=500, eps=0.05):
-    if f(xl)*f(xu) >0:
+def bisection(f, xl, xu, max_itr=500, eps=0.05):
+    if f(xl)*f(xu) > 0:
         print("Wrong guess!")
         return None
-    
-    if f(xl)*f(xu)==0:
-        if f(xl) == 0:
-            return xl
-        else:
-            return xu
 
-    iter = 1
+    if f(xl) == 0:
+        return xl
+    if f(xu) == 0:
+        return xu
+
+    itr = 1
     xr_old = (xl + xu) / 2
 
     while True:
@@ -27,16 +26,24 @@ def Bisection(f, xl, xu, max_itr=500, eps=0.05):
 
         xr_new = (xl + xu) / 2
         ae = fabs(xr_new - xr_old)
-        xr_old =  xr_new
-        
-        itr=itr+1
-        if ae<=eps or itr>max_itr:
+        xr_old = xr_new
+
+        itr = itr + 1
+        if ae <= eps or itr > max_itr:
             break
-    return xr_old 
+    return xr_old
 
-xl, xu = 1,3
-xr = Bisection(f,xl,xu)
 
-if xr is not None:
-    print(f"The root of the function is : {xr:.3f}")
-    print(f"The value of the function us ")
+te = int(input("Test case : "))
+while te:
+    xl = int(input("xl : "))
+    xu = int(input("xu : "))
+    xr = bisection(f, xl, xu)
+
+    if xr is not None:
+        print(f"The root of the function is : {xr:.3f}")
+        print(f"The value of the function is : {f(xr):.3f}")
+    else:
+        print("No root found.")
+
+    te -= 1
