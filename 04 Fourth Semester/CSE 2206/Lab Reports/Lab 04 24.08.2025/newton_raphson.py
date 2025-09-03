c56c7d49cf0f2ac140001856cb834a1669af0f8b
atif721 <abdullahatif721@outlook.com>
2025-09-03 09:59:03 +0600

    lecture and lab updated

--- 04 Fourth Semester/CSE 2206/Lab Reports/Lab 04 24.08.2025/newton_raphson.py
+++ 04 Fourth Semester/CSE 2206/Lab Reports/Lab 04 24.08.2025/newton_raphson.py
@@ -1,35 +1,47 @@
-from math import fabs
+from math import fabs, exp
 
-def f(x):
-    return x*x*x - 2*x -5
 
-def f_deriv(x):
-    return 3*x*x -2
+def func(x):
+    return x * exp(x) - 1
 
-def newton_raphson(f, f_deriv, x0, max_itr=500, eps=0.05):
-    if(f_deriv(x0) ==0 ):
-        print("Change the guess!")
-        return 
-    itr = 1
-    xr_old = x0
-    
-    
-    while True:
-        if(f_deriv(xr_old) == 0):
-            print("Change the guess!")
-            return
-    
-        xr_new = xr_old - (f(xr_old) / f_deriv(xr_old))
-        ae = fabs(xr_new - xr_old)
-        xr_old = xr_new
-        itr +=1
-        if(ae<= eps or itr>max_itr):
-            break
-    return xr_old
 
-x0 = float(input("Enter inital guess : "))
+def dfunc(x):
+    return exp(x) * (1 + x)
 
-root = newton_raphson(f, f_deriv, x0)
 
-print(f"The root of the given equation is : {root:.5f}")
-print(f"The value of f(root) : {f(root):.5f}")
+def newton_raphson(func, deriv_f, x0, max_iter=500, eps=1e-6):
+
+    if deriv_f(x0) == 0:
+        print("Zero derivative at initial guess. Change guess.")
+        return None
+
+    x = x0
+    for i in range(1, max_iter + 1):
+        df_val = deriv_f(x)
+        if df_val == 0:
+            print("Zero derivative occured. Method fails.")
+            return None
+
+        x_new = x - func(x) / df_val
+        error = fabs(x_new - x)
+
+        print(f"Iteration {i}: ")
+        print(f"x = {x_new:.4f}, f(x) = {func(x_new):.4f},")
+        print(f"error = {error:.4e}\n")
+
+        if error <= eps:
+            return x_new
+
+        x = x_new
+
+    print("Maximum iterations reached.")
+    return x
+
+
+if __name__ == "__main__":
+    guess = float(input("Enter initial guess: "))
+    root = newton_raphson(func, dfunc, guess)
+
+    if root is not None:
+        print(f"The root of the given equation is = {root:.4f}")
+        print(f"The value of f(root) = {func(root):.4e}")
---
+++ 04 Fourth Semester/CSE 2206/Lab Reports/Lab 05 31.08.2025/secant method.py
@@ -0,0 +1,40 @@
+from math import fabs
+
+
+def f(x):
+    return x**3 - 2*x - 5
+
+
+def Secant(f, x0, x1, max_itr=500, eps=0.005):
+    itr = 1
+
+    while True:
+        f0 = f(x0)
+        f1 = f(x1)
+
+        if (f1-f0 == 0):
+            print("Denominator becomes zero. Try different")
+            return None
+
+        x2 = x1 - (f1*(x1-x0) / (f1-f0))
+
+        ae = fabs(x2-x1)
+
+        x0 = x1
+        x1 = x2
+        itr += 1
+
+        if (ae <= eps or itr > max_itr):
+            break
+
+    return x1
+
+
+x0 = 4
+x1 = 5
+
+xr = Secant(f, x0, x1)
+
+if xr is not None:
+    print(f"The root is at {xr:.4f}")
+    print(f"f(xr) = {f(xr):.4f}")
