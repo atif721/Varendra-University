5bee0583325467438b8fdb3149bcf56faead53fc
atif721 <abdullahatif721@outlook.com>
2025-09-03 22:30:42 +0600

    lab updated

---
+++ 04 Fourth Semester/CSE 2206/Lab Reports/Lab 1 26.07.2025/problem_01.py
@@ -0,0 +1,4 @@
+i = 1
+name = "Abdullah Atif"
+for i in range(0, 10):
+    print(f"{i+1:>2}. {name}")
--- 04 Fourth Semester/CSE 2206/Lab Reports/Lab 2 02.08.2025/error_analysis.py
+++ 04 Fourth Semester/CSE 2206/Lab Reports/Lab 2 02.08.2025/error_analysis.py
@@ -2,12 +2,13 @@ def error_analysis(true_value, approx_value):
     absolute_error = abs(true_value - approx_value)
     relative_error = absolute_error/abs(true_value)
     percentage_errror = relative_error*100
-    
+
     return absolute_error, relative_error, percentage_errror
+
 
 true_value = float(3.1416)
 approx_value = float(3.14)
-ae,re,pe= error_analysis(true_value,approx_value)
+ae, re, pe = error_analysis(true_value, approx_value)
 
 print(f"Absolute Error: {ae:.5f}")
 print(f"Relative Error: {re:.5f}")
