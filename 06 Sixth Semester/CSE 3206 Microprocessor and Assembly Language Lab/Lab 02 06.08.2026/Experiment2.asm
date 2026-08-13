7df198ee86ea2da0a76afc284036dd9443b9cace
atif721 <abdullahatif721@outlook.com>
2026-08-13 23:05:18 +0600

    experiment 1,2 files added

---
+++ 06 Sixth Semester/CSE 3206 Microprocessor and Assembly Language Lab/Lab 02 06.08.2026/Experiment2.asm
@@ -0,0 +1,27 @@
+.model small
+.stack 100h
+.code 
+
+main proc
+    mov ah,1
+    int 21h     ;input
+    mov bl,al
+    
+    
+    mov ah,2
+    mov dl,10
+    int 21h     ;new line
+    mov dl,13
+    int 21h
+          
+          
+    mov ah,2
+    mov dl,bl   ;output
+    int 21h  
+    
+    
+    exit:
+    mov ah,4ch
+    int 21h     ;return
+    main endp
+ end main
