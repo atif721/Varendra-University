27e79027e706c92d9b51ba30f69f3a95ffcc6c8a
atif721 <abdullahatif721@outlook.com>
2026-09-12 19:11:15 +0600

    web programming file added

---
+++ 06 Sixth Semester/CSE 3210 E-commerce and Web Programming Project Lab/Lab_09.09.2026/myphp.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+  echo("Hello Wordl!");
+  echo("Hello Wordl!");
+  echo("Hello Wordl!");
+  echo("Hello Wordl!");
+  for($i=0; $i<10; $i++){
+    echo("<br> my name ");
+  }
+
+?>
+</body>
+</html>
